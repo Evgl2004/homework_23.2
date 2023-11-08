e8ef0602fa5a82f46aecbfb7863b4c1c272aa102
Andrey Evgl <evgl2004@yandex.ru>
2023-11-08 17:57:33 +0500

    Изменение 03. Реализация общего (базового) шаблона, шаблона со списком продуктов, шаблона с контактами, подшаблона с меню.

--- main/views.py
+++ main/views.py
@@ -8,20 +8,24 @@ def home(request):
     content = {
         'object_list': Product.objects.all(),
         'title': 'Главная',
+        'description': 'Вся информация о товаре',
     }
-    # for product_item in Product.objects.all().order_by('-pk')[:5]:
-    #     print(product_item)
 
     return render(request, 'main/home.html', content)
 
 
 def contacts(request):
 
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
+    # if request.method == 'POST':
+    #     name = request.POST.get('name')
+    #     phone = request.POST.get('phone')
+    #     message = request.POST.get('message')
+    #
+    #     print(f"{name} - {phone} - {message}")
 
-        print(f"{name} - {phone} - {message}")
+    content = {
+        'title': 'Контакты',
+        'description': 'Наша контактная информация',
+    }
 
-    return render(request, 'main/contacts.html')
+    return render(request, 'main/contacts.html', content)

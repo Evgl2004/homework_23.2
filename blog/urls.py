4b9310e83de36f5ce06aaa1f52ed0c11710d824e
Andrey Evgl <evgl2004@yandex.ru>
2023-11-20 21:12:14 +0500

    Изменение 03. Выполнено Задание №3: модифицирован вывод и обработка запросов.

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,7 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='create'),
     path('list/', BlogListView.as_view(), name='list'),
-    path('detail/<int:pk>', BlogDetailView.as_view(), name='detail'),
+    path('view/<int:pk>', BlogDetailView.as_view(), name='view'),
     path('edit/<int:pk>', BlogUpdateView.as_view(), name='edit'),
     path('delete/<int:pk>', BlogDeleteView.as_view(), name='delete'),
 ]

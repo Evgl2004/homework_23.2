4c8c9a38e8f052c341f71b373b8ebf818d10b091
Andrey Evgl <evgl2004@yandex.ru>
2023-12-06 19:15:41 +0500

    Изменение 01. Выполнено Задание №1: для модели продуктов реализован механизм CRUD. Добавлены условия для создания новых продуктов (ограничивающий список слов для описания и наименования продуктов).

---
+++ main/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+
+from main.models import Product
+
+
+class ProductFrom(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = ('name', 'description', 'image_preview', 'category', 'price')
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+        bad_word = ('казино', 'криптовалюта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар')
+
+        for word in bad_word:
+            if cleaned_data.lower().find(word.lower()) != -1:
+                raise forms.ValidationError(f'В наименовании использовано запрещенное слово: {word}')
+
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+        bad_word = ('казино', 'криптовалюта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар')
+
+        for word in bad_word:
+            if cleaned_data.lower().find(word.lower()) != -1:
+                raise forms.ValidationError(f'В описании использовано запрещенное слово: {word}')
+
+        return cleaned_data
+
--- main/views.py
+++ main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView, TemplateView
+from django.views.generic import CreateView, UpdateView, ListView, DetailView, DeleteView, TemplateView
 
 from main.models import Product, Category
+
+from main.forms import ProductFrom
+
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
@@ -34,4 +38,45 @@ class CategoryProductView(TemplateView):
         context_data['description'] = f"Список продуктов {Category.objects.get(pk=self.kwargs.get('pk')).name}",
 
         return context_data
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductFrom
+    success_url = reverse_lazy('main:home')
+
+    extra_context = {
+        'title': 'Добавить продукт',
+        'description': 'Новый продукт',
+    }
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductFrom
+    success_url = reverse_lazy('main:home')
+
+    extra_context = {
+        'title': 'Изменить продукт',
+        'description': 'Редактировать продукт',
+    }
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
+    extra_context = {
+        'title': 'Просмотр продукта',
+        'description': 'Информация о продукте',
+    }
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('main:home')
+
+    extra_context = {
+        'title': 'Удалить продукт',
+        'description': 'Подтверждение удаление продукта',
+    }
 
